abdab33662d47afa83d360bddfd9df10001d8974
TohaDoku <dokuchaevantoha@gmail.com>
2024-08-14 14:41:42 +0300

    Сделал карусель, приступил к отзывам

--- js/script.js
+++ js/script.js
@@ -16,3 +16,41 @@ span.onclick = function() {
     span.classList.remove("active");
     btn.classList.remove("hidden");
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const slides = document.querySelectorAll('.reviews__slide img');
+    let currentIndex = 2; // Индекс активного слайда
+    const totalSlides = slides.length;
+
+    const images = [
+        './img/review.png',
+        './img/logo.png',
+        './img/boy.png',
+        './img/review.png',
+        './img/review.png'
+    ];
+
+    function updateSlides() {
+        for (let i = 0; i < totalSlides; i++) {
+            const index = (currentIndex + i - 2 + images.length) % images.length;
+            slides[i].src = images[index];
+        }
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % images.length;
+        updateSlides();
+    }
+
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        updateSlides();
+    }
+
+    document.querySelector('.reviews__slider_next').addEventListener('click', nextSlide);
+    document.querySelector('.reviews__slider_prev').addEventListener('click', prevSlide);
+
+    updateSlides(); // Инициализация начальных изображений
+});
+
+
